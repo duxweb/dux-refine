c3f93d1f38fa285c9ebaac2920212b50110acf71
duxphp <admin@duxphp.com>
2023-08-14 22:53:26 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
-import typescript from '@rollup/plugin-typescript'
+import { buildPlugin } from 'vite-plugin-build'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -9,28 +9,24 @@ export default defineConfig({
   plugins: [
     react(),
     UnoCSS(),
-    typescript({
-      target: 'es5',
-      rootDir: resolve('src/'),
-      declaration: true,
-      declarationDir: resolve('dist'),
-      exclude: resolve('node_modules/**'),
-    }),
-  ],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'dux-ui',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'unocss'],
-      output: {
-        globals: {
-          react: 'react',
-          'react-dom': 'react-dom',
+    buildPlugin({
+      fileBuild: {
+        emitDeclaration: true,
+      },
+      libBuild: {
+        buildOptions: {
+          rollupOptions: {
+            external: ['react'],
+            output: { globals: { react: 'React' } },
+          },
+          lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'RbacComponents',
+            fileName: (format) => `rbac-components.${format}.js`,
+          },
         },
       },
-    },
-  },
+      
+    }),
+  ],
 })
