66d4a236eefe32f8fa3fea90e7048d258f165faa
duxphp <admin@duxphp.com>
2023-08-15 10:27:21 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
-import { buildPlugin } from 'vite-plugin-build'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    UnoCSS(),
-    buildPlugin({
-      fileBuild: {
-        emitDeclaration: true,
-        esOutputDir: 'dist',
-        commonJsOutputDir: false,
-      },
-    }),
-  ],
+  plugins: [react(), UnoCSS()],
 })
