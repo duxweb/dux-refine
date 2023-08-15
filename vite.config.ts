8b01e326a6c741a24ee342946e3536097151569c
duxphp <admin@duxphp.com>
2023-08-15 11:25:50 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import {buildPlugin} from 'vite-plugin-build'
 import { resolve } from 'path'
-import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'dux',
-      fileName: 'index',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: Object.keys(pkg.dependencies),
-    },
-  },
+  plugins: [react(), UnoCSS(), buildPlugin({
+    fileBuild: 
+  })],
 })
