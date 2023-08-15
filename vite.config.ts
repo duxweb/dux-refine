60f5126573a0870c652e33b42a963afe885ec22e
duxphp <admin@duxphp.com>
2023-08-15 10:23:43 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import { buildPlugin } from 'vite-plugin-build'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
