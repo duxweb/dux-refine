76931dafdbdd5c60d7964f791756218259f833b2
duxweb <admin@dux.plus>
2023-08-17 18:03:39 +0800

    update

--- src/core/i18n.ts
+++ src/core/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next'
-import detector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import detector from 'i18next-browser-languagedetector'
 import enTranslation from '../locales/en/common.json'
 import zhTranslation from '../locales/zh/common.json'
 
