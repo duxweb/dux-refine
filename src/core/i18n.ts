85a4e7dca76857d71ab877d38b554812546c7d15
duxweb <admin@dux.plus>
2023-08-17 18:00:58 +0800

    update

--- src/core/i18n.ts
+++ src/core/i18n.ts
@@ -1,21 +1,25 @@
 import i18n from 'i18next'
+import detector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 import enTranslation from '../locales/en/common.json'
 import zhTranslation from '../locales/zh/common.json'
 
-i18n.use(initReactI18next).init({
-  supportedLngs: ['en', 'zh'],
-  resources: {
-    en: {
-      common: enTranslation,
+i18n
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'zh'],
+    resources: {
+      en: {
+        common: enTranslation,
+      },
+      zh: {
+        common: zhTranslation,
+      },
     },
-    zh: {
-      common: zhTranslation,
-    },
-  },
-  ns: ['common'],
-  defaultNS: 'common',
-  fallbackLng: ['en', 'zh'],
-})
+    ns: ['common'],
+    defaultNS: 'common',
+    fallbackLng: ['en', 'zh'],
+  })
 
 export default i18n
