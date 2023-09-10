b2fc8f648e07939fd7528f79e91a84705e2b7e04
duxphp <admin@duxphp.com>
2023-09-10 09:34:25 +0800

    perf(i18n): add Default Adaptive Language

--- src/provider/i18n.ts
+++ src/provider/i18n.ts
@@ -4,6 +4,9 @@ import detector from 'i18next-browser-languagedetector'
 import enTranslation from '../locales/en/common.json'
 import zhTranslation from '../locales/zh/common.json'
 
+const userLanguage = navigator.language
+const mainLanguage = userLanguage.split('-')[0]
+
 i18n
   .use(detector)
   .use(initReactI18next)
@@ -19,9 +22,10 @@ i18n
     },
     ns: ['common'],
     defaultNS: 'common',
+    lng: mainLanguage,
     fallbackLng: ['en', 'zh'],
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
   })
 
