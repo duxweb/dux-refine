7c6b400dce20227bae19cb7aa12c38d3576215d1
duxweb <admin@dux.plus>
2024-01-05 18:00:08 +0800

    update

--- src/components/data/listform.tsx
+++ src/components/data/listform.tsx
@@ -1,6 +1,14 @@
-import { Button, EnhancedTable, PrimaryTableCol } from 'tdesign-react/esm'
+import { Button, EnhancedTable, PrimaryTableCol, PrimaryTableCellParams } from 'tdesign-react/esm'
 import { useControllableValue } from 'ahooks'
-import { useMemo, createContext, cloneElement, ReactElement } from 'react'
+import {
+  useMemo,
+  createContext,
+  cloneElement,
+  ReactElement,
+  ReactNode,
+  SetStateAction,
+} from 'react'
+import { useTranslate } from '@refinedev/core'
 
 const context = createContext({})
 
@@ -8,7 +16,11 @@ export interface ListformData {
   title: string
   field: string
   width?: string | number
-  component: ReactElement
+  component?: ReactElement
+  content?: (
+    cell: PrimaryTableCellParams<Record<string, any>>,
+    setValue: (v: SetStateAction<Record<string, any>[]>, ...args: any[]) => void
+  ) => ReactNode
 }
 
 export interface ListformProps {
@@ -16,9 +28,19 @@ export interface ListformProps {
   defaultValue?: Record<string, any>[]
   onChange?: (value: Record<string, any>[]) => void
   options: ListformData[]
+  createAction?: boolean
+  deleteAction?: boolean
+  createCallback?: (value: Record<string, any>[]) => void
 }
 
-export const Listform = ({ options, ...props }: ListformProps) => {
+export const Listform = ({
+  options,
+  createAction = true,
+  deleteAction = true,
+  createCallback,
+  ...props
+}: ListformProps) => {
+  const translate = useTranslate()
   const [value, setValue] = useControllableValue<Record<string, any>[]>(props)
 
   const columns = useMemo<PrimaryTableCol[]>(() => {
@@ -27,50 +49,68 @@ export const Listform = ({ options, ...props }: ListformProps) => {
         title: item.title,
         colKey: item.field,
         width: item.width,
-        cell: ({ row, rowIndex }) => {
-          return cloneElement(item.component, {
-            value: row[item.field],
-            onChange: (v: any) => {
-              setValue((old) => {
-                const rows = old[rowIndex] || {}
-                rows[item.field] = v
-                return [...old]
+        cell: (cell: PrimaryTableCellParams<Record<string, any>>) => {
+          const { row, rowIndex } = cell
+          return item.component
+            ? cloneElement(item.component, {
+                value: row[item.field],
+                onChange: (v: any) => {
+                  setValue((old) => {
+                    const rows = old[rowIndex] || {}
+                    rows[item.field] = v
+                    return [...old]
+                  })
+                },
               })
-            },
-          })
+            : item?.content?.(cell, setValue)
         },
       }
     })
+    if (!createAction && !deleteAction) {
+      return cols
+    }
+
     cols.push({
       colKey: 'op',
       align: 'center',
       title: (
-        <Button
-          shape='circle'
-          theme='default'
-          variant='outline'
-          icon={<div className='i-tabler:plus h-4 w-4'></div>}
-          onClick={() => {
-            setValue((old) => {
-              return [...(old || []), {}]
-            })
-          }}
-        />
+        <>
+          {createAction ? (
+            <Button
+              shape='circle'
+              theme='default'
+              variant='outline'
+              icon={<div className='i-tabler:plus h-4 w-4'></div>}
+              onClick={() => {
+                setValue((old) => {
+                  const list = old || []
+                  return [list, createCallback?.(list) || {}]
+                })
+              }}
+            />
+          ) : (
+            translate('table.actions')
+          )}
+        </>
       ),
       cell: ({ rowIndex }) => {
         return (
-          <Button
-            shape='circle'
-            theme='danger'
-            variant='outline'
-            icon={<div className='i-tabler:trash h-4 w-4'></div>}
-            onClick={() => {
-              setValue((v) => {
-                v.splice(rowIndex, 1)
-                return [...v]
-              })
-            }}
-          />
+          <>
+            {deleteAction && (
+              <Button
+                shape='circle'
+                theme='danger'
+                variant='outline'
+                icon={<div className='i-tabler:trash h-4 w-4'></div>}
+                onClick={() => {
+                  setValue((v) => {
+                    v.splice(rowIndex, 1)
+                    return [...v]
+                  })
+                }}
+              />
+            )}
+          </>
         )
       },
       width: 70,
